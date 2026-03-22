ad02677f731dfc178ea0066300255260788f70c0
Jeremy Swartwood <jeremy.swartwood@gmail.com>
2026-03-21 23:38:58 -0800

    Fix XNA preset short name to match full name

--- apps/KernSmith.Ui/Models/EnginePreset.cs
+++ apps/KernSmith.Ui/Models/EnginePreset.cs
@@ -14,7 +14,7 @@ public static class EnginePresets
 {
     public static readonly EnginePreset Unity = new("Unity", "UY", 2048, 2048, true, true, 1, 1, "Xml", true, "Unity TextMesh Pro compatible");
     public static readonly EnginePreset Godot = new("Godot", "GD", 1024, 1024, true, true, 1, 1, "Text", true, "Godot BMFont import compatible");
-    public static readonly EnginePreset MonoGame = new("XNA", "MG", 1024, 1024, true, true, 1, 1, "Xml", true, "MonoGame/XNA SpriteFont compatible");
+    public static readonly EnginePreset MonoGame = new("XNA", "XNA", 1024, 1024, true, true, 1, 1, "Xml", true, "MonoGame/XNA SpriteFont compatible");
     public static readonly EnginePreset Unreal = new("Unreal", "UR", 2048, 2048, true, true, 2, 2, "Text", true, "Unreal Engine UMG compatible");
     public static readonly EnginePreset Phaser = new("Phaser", "PH", 512, 512, true, true, 1, 1, "Xml", false, "Phaser.js bitmap font compatible");
     public static readonly EnginePreset Custom = new("Custom", "CU", 1024, 1024, true, true, 1, 1, "Text", true, "Custom configuration");
